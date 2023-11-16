8cffe7f2aebc94f51076f740a909e96695f7ff76
AlaaHamdyMohammadi <alaahamdy2197@gmail.com>
2023-11-16 21:07:24 +0200

    Install bcrypt

--- pages/api/auth/signup.js
+++ pages/api/auth/signup.js
@@ -1,5 +1,21 @@
-// To creating new user
-function handler(req, res){
+import { connectToDatabase } from "../../../lib/db";
 
+// To creating new user
+async function handler(req, res){
+    const {email, password} = req.body;
+
+    // validation
+    if(!email || !email.includes('@') || !password || password.length <= 5){
+        res.status(422).json({message: 'Invalid data'});
+        return;
+    }
+
+    const newUser = {
+        email, password
+    }
+
+    const client = await connectToDatabase();
+    const db = client.db();
+    db.collection('users').insertOne(newUser);
 }
 export default handler;
